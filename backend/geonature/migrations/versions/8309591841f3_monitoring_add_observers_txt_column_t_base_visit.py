a4e6bc057ea8b80d69332fc10d5955c3961bc2e8
Mathieu ROUDAUT <mroudaut@creamontblanc.org>
2024-06-07 06:54:29 +0200

    feat(monitorings): migrate monitorings table declaration inside GN

--- backend/geonature/migrations/versions/8309591841f3_monitoring_add_observers_txt_column_t_base_visit.py
+++ backend/geonature/migrations/versions/8309591841f3_monitoring_add_observers_txt_column_t_base_visit.py
@@ -6,9 +6,8 @@ Create Date: 2023-10-06 11:07:43.532623
 
 """
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "8309591841f3"
